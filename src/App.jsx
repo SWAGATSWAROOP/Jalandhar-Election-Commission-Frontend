13d210cff00fb52e34f51d5cd39737f5873a970f
Satyam Gupta <guptasatyam1709@gmail.com>
2024-05-22 10:06:47 +0530

    testing merge issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Navbar from './component/Navbar';
 import Footer from './component/Footer';
 import Main from './component/Main';
@@ -6,14 +5,11 @@ function App() {
   return (
     <>
       <Navbar/> 
+      <h2>My Satyam app js</h2>
       <Main/>
       <Footer/>
     </>
   )
-=======
-function App() {
-  return <></>;
->>>>>>> 0ceabd536913c522acb5b8f3411cfd3a10ef7bc7
 }
 
 export default App;
